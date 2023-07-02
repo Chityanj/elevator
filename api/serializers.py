2204865d7137c233417eee3202d44919543c68c1
Chityanj <chityanj@gmail.com>
2023-07-02 21:39:11 +0530

    Fix derp and refactor

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Elevator
+from api.models import Elevator
 
 class ElevatorSerializer(serializers.ModelSerializer):
     class Meta:
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import action
-from .models import Elevator
-from .serializers import ElevatorSerializer
+from api.models import Elevator
+from api.serializers import ElevatorSerializer
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
